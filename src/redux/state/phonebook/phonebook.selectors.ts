1c25b693d48699a3040be12d07b00c19438d5adb
Manish Kumar <87manishkumar@gmail.com>
2023-10-24 23:19:04 +0530

    removed redux toolkit

--- src/redux/state/phonebook/phonebook.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit"
-import type { RootState } from "../../types"
-import { selectAll } from "./phonebook.slice"
-
-const phonebookState = (state: RootState) => state.phonebook
-
-export const selectPhonebook = createSelector(phonebookState, (state) =>
-  selectAll(state),
-)
